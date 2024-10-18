3f5fb873cac498445536253e9a11e78d06f27e59
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-17 21:05:55 -0600

    Dia 2: Creacion del personaje y agregarlo en Ventana

---
+++ personaje.py
@@ -0,0 +1,16 @@
+#imports
+import pygame
+
+class Cubo:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 1
+        self.color = "red"
+        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Forma de crear el personaje
+# Funcion que dibujara el personaje
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana,self.color,self.rect)
+
--- pygame1.py
+++ pygame1.py
@@ -1,7 +1,7 @@
 # Dia @: 
 #imports
 import pygame
-
+from personaje import Cubo
 ANCHO = 1000    # Ancho de la ventana
 ALTO = 800      # Alto de la ventana
 VENTANA = pygame.display.set_mode([ANCHO,ALTO]) # Ventana principal con un arreglo como parametros
@@ -9,12 +9,16 @@ VENTANA = pygame.display.set_mode([ANCHO,ALTO]) # Ventana principal con un arreg
 # Bucle principal del juego
 juagando = True
 
+cubo = Cubo(100,100)#   Dibujara el personaje en la ventana principal
+
 while juagando:
     eventos = pygame.event.get()
 
     for evento in eventos:
         if evento.type == pygame.QUIT:
             juagando = False
+
+    cubo.dibujar(VENTANA)
 
     pygame.display.update()
 
