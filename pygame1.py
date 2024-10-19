012264b84dfb33debc2422e04d3b09255dd68b4d
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-18 21:29:09 -0600

    Dia 3: Gestionar teclas de entrada pygame

--- pygame1.py
+++ pygame1.py
@@ -11,8 +11,24 @@ juagando = True
 
 cubo = Cubo(100,100)#   Dibujara el personaje en la ventana principal
 
+def gestionar_teclas(teclas):
+    if teclas[pygame.K_w]:
+        cubo.y -= cubo.velocidad
+    if teclas[pygame.K_s]:
+        cubo.y == cubo.velocidad
+    if teclas[pygame.K_a]:
+        cubo.x -= cubo.velocidad
+    if teclas[pygame.K_w]:
+        cubo.x += cubo.velocidad
+
+
 while juagando:
     eventos = pygame.event.get()
+
+    teclas = pygame.key.get_pressed()
+
+    gestionar_teclas(teclas)
+
 
     for evento in eventos:
         if evento.type == pygame.QUIT:
