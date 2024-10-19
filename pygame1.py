83d06033f6986be3268465394f354fb40e3bb8ed
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-18 21:41:06 -0600

    Dia 3: Gestionar teclas Documentacion y cambio de errores pygame

--- pygame1.py
+++ pygame1.py
@@ -11,23 +11,23 @@ juagando = True
 
 cubo = Cubo(100,100)#   Dibujara el personaje en la ventana principal
 
-def gestionar_teclas(teclas):
-    if teclas[pygame.K_w]:
-        cubo.y -= cubo.velocidad
-    if teclas[pygame.K_s]:
-        cubo.y == cubo.velocidad
-    if teclas[pygame.K_a]:
-        cubo.x -= cubo.velocidad
-    if teclas[pygame.K_w]:
-        cubo.x += cubo.velocidad
+def gestionar_teclas(teclas):   #Funcion para gestionar las teclas que controlaran al personaje
+    if teclas[pygame.K_w]:  # Detecta si la tecla deleccionada esta siendo presionada
+        cubo.y -= cubo.velocidad    # Si "w" es presionada resta la posision de y. Resta 1 que es la velocidad
+    if teclas[pygame.K_s]:  # Detecta si la tecla deleccionada esta siendo presionada
+        cubo.y += cubo.velocidad    # Si "s" es presionada suma la posision de y. Suma 1 que es la velocidad
+    if teclas[pygame.K_a]:  # Detecta si la tecla deleccionada esta siendo presionada
+        cubo.x -= cubo.velocidad    # Si "a" es presionada resta la posision de x. Resta 1 que es la velocidad
+    if teclas[pygame.K_d]:  # Detecta si la tecla deleccionada esta siendo presionada
+        cubo.x += cubo.velocidad    # Si "d" es presionada suma la posision de x. Suma 1 que es la velocidad
 
 
 while juagando:
     eventos = pygame.event.get()
 
-    teclas = pygame.key.get_pressed()
-
-    gestionar_teclas(teclas)
+    teclas = pygame.key.get_pressed()   # Esto devolvera una lista con todas las teclas que sean presionadas
+                    #"key es para acceder a todo lo que tenga que ver con el teclado"
+    gestionar_teclas(teclas)    # Llamada a la funcion
 
 
     for evento in eventos:
