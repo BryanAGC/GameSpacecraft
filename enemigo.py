b5e3f601ce78a43202d74fb3f103764af050eca8
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-19 21:21:08 -0600

    Dia 4: Enemigo documentacion y correccion de errores

--- enemigo.py
+++ enemigo.py
@@ -1,3 +1,6 @@
+# La clase enemigo es una replica hasta el momento de personaje.py
+# con la diferencia de sus caracteristicas visuales
+# y uan nueva funcion de moviemieno
 #imports
 import pygame
 
@@ -5,15 +8,15 @@ class Enemigo:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.ancho = 50
-        self.alto = 50
+        self.ancho = 75
+        self.alto = 75
         self.velocidad = .33
         self.color = "purple"
-        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) 
+        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Forma de crear el personaje
 
     def dibujar(self, ventana):
         pygame.draw.rect(ventana,self.color,self.rect)
-        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) 
-        
-    def movimiento(self):
-        self.y += self.velocidad
+        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Cambiara las coordenadas de enemigo cada vez que sea dibujado
+
+    def movimiento(self):   # funcion de miviento que permitira que el personaje se mueva automaticamente 
+        self.y += self.velocidad        # Para que la funcion duncione debe ser agregada a pygame1
