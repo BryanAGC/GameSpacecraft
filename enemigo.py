47b7ca138f91ec8377abaeef6ebb24568337862b
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-30 20:12:02 -0600

    Dia 14: Agregar images al personaje y al enemigo

--- enemigo.py
+++ enemigo.py
@@ -14,10 +14,12 @@ class Enemigo:
         self.color = "purple"
         self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Forma de crear el personaje
         self.vida = 3
-        
+        self.imagen = pygame.image.load("GameSpacecraft\\alien1.png")
+        self.imagen=pygame.transform.scale(self.imagen,(self.ancho,self.alto))
     def dibujar(self, ventana):
-        pygame.draw.rect(ventana,self.color,self.rect)
+        # pygame.draw.rect(ventana,self.color,self.rect) #! Para que no salga el cuadrado pero si la imagen
         self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Cambiara las coordenadas de enemigo cada vez que sea dibujado
-
+        ventana.blit(self.imagen,(self.x, self.y))
+        
     def movimiento(self):   # funcion de miviento que permitira que el personaje se mueva automaticamente 
         self.y += self.velocidad        # Para que la funcion duncione debe ser agregada a pygame1
--- personaje.py
+++ personaje.py
@@ -10,7 +10,10 @@ class Cubo:
         self.velocidad = 10 # Auentamos la velocidad debido al cambio en FPS
         self.color = "red"
         self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Forma de crear el personaje
+        self.imagen = pygame.image.load("GameSpacecraft\\nave1.png")
+        self.imagen=pygame.transform.scale(self.imagen,(self.ancho,self.alto))
 # Funcion que dibujara el personaje
     def dibujar(self, ventana):
-        pygame.draw.rect(ventana,self.color,self.rect)
+        #pygame.draw.rect(ventana,self.color,self.rect) #! Se comenta para que solo apresca la nave pero sigue estando ahi
         self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto) #Cambiara las coordenadas de perosonaje cada vez que sea dibujado
+        ventana.blit(self.imagen,(self.x, self.y))
